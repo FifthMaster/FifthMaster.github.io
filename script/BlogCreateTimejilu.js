f3218bcc0917e6de79fb0025df3219eb00bbc976
FifthMaster <143803859+FifthMaster@users.noreply.github.com>
2023-09-15 17:12:09 +0800

    Add files via upload

--- script/BlogCreateTimejilu.js
+++ script/BlogCreateTimejilu.js
@@ -1,13 +1,13 @@
-var seconds = 0; // 初始化秒数
+// 博客创建日期（2023/9/14）
+const blogCreatedDate = new Date("2023-09-14");
 
-// 更新计时器显示的函数
-function updateTimer() {
-    document.getElementById("timer").innerHTML = seconds + " 秒";
-    seconds++;
-}
+// 获取当前日期
+const currentDate = new Date();
 
-// 自动开始计时
-window.onload = function() {
-    // 每隔一秒执行一次updateTimer函数
-    setInterval(updateTimer, 1000);
-};
+// 计算日期差（以天为单位）
+const timeDifference = currentDate - blogCreatedDate;
+const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+// 将日期差显示在网页上
+document.getElementById("dateDifference").innerHTML = `博客创建于 2023年9月14日，距离现在已经过去了 ${daysDifference} 天。`;
+ById("dateDifference").innerHTML = `博客创建于 2023年9月14日，距离现在已经过去了 ${daysDifference} 天。`;
